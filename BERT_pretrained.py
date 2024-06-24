d265d079f0586d7e3d1d5a4ea1ae44686f9b5162
Krikor Postalian <146595608+krikorantranik@users.noreply.github.com>
2024-06-24 10:47:07 -0700

    Create BERT_pretrained.py

---
+++ BERT_pretrained.py
@@ -0,0 +1,9 @@
+#
+
+from sentence_transformers import SentenceTransformer
+
+#download model
+bertmodel = SentenceTransformer('all-mpnet-base-v2')
+
+#create embdedding from pandas column
+reviews_embedding = bertmodel.encode(maindataset["NLPtext"])
